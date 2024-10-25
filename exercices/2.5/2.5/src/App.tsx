b699edd8f732e8e6234d7521037468b6e42decde
alexandre-topet-vinci <alexandre.topet@student.vinci.be>
2024-10-25 09:46:15 +0200

    new:2.5-destructuring

--- exercices/2.5/2.5/src/App.tsx
+++ exercices/2.5/2.5/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   const ClickCounter = () => {
-    setCount((count) => count + 1)
+    setCount((count) => count + 1);
   }
 
   return (
