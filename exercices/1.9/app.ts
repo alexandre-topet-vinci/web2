f51d19cd3b945d03c649382321a358bef15a5521
alexandre-topet-vinci <alexandre.topet@student.vinci.be>
2024-10-25 09:10:37 +0200

    new:ex2.5-init

--- exercices/1.9/app.ts
+++ exercices/1.9/app.ts
@@ -1,22 +1,26 @@
 import express from "express";
 
-import pizzaRouter from "./routes/films";
+import textRouter from "./routes/texts";
+import { requestCounterMiddleware } from "./utils/counter";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-let getCounter = 0;
-
+/* Middleware to count the number of GET requests */
+let requestCount = 0;
 app.use((req, _res, next) => {
   if (req.method === "GET") {
-    getCounter++;
-    console.log(`GET counter : ${getCounter}`);
+    requestCount++;
+    console.log(`GET counter : ${requestCount}`);
   }
   next();
 });
 
-app.use("/films", pizzaRouter);
+/* Challenge of ex1.2 */
+app.use(requestCounterMiddleware);
+
+app.use("/texts", textRouter);
 
 export default app;
